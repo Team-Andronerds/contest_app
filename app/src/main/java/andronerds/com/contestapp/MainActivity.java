5d284defc8e9c1d2a1c853061a3140d540f5e0a8
Chris G. Portokalis <cpsteeler@yahoo.com>
2015-01-30 12:40:40 -0600

    Updated nav drawer to follow material design. Changed style to NoActionBar Added Toolbar in activity_main Hello World Text View is appearing behind toolbar for some reason, Will Fix.

--- app/src/main/java/andronerds/com/contestapp/MainActivity.java
+++ app/src/main/java/andronerds/com/contestapp/MainActivity.java
@@ -1,20 +1,36 @@
 package andronerds.com.contestapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends NavDrawerActivity
 {
+    /*Chris Portokalis
 
-    @Override
+        On Create being handled by the NavDrawerActivity Abstract Class
+    */
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+    }*/
+
 
 
     @Override
